b0fc8be81ade9924335a987ca64bcd53316701f1
sungaee <sungaee121@naver.com>
2023-10-05 15:50:21 +0900

    [react-masterclass] style-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Father>
+      <Btn>Login</Btn>
+    </Father>
+  )
 }
 
 export default App;
