bc853dac61bae62cea1e5561a8291c2c2832d652
sungaee <sungaee121@naver.com>
2024-11-28 16:46:19 +0900

    [typescript] Form 다루는방법과 submit이벤트 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import styled from "styled-components"
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
-  return <div>
-      <Circle  borderColor="yellow" bgColor="teal"/>
-      <Circle bgColor="tomato"></Circle>
+  const [value, setValue] = useState("")
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value); // 상태 업데이트
+    const { currentTarget: { value } } = event;
+    setValue(value)
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value)
+  }
+   
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username" />
+        <button>Log in</button>
+      </form>
     </div>
+  )
 }
 
 export default App;
